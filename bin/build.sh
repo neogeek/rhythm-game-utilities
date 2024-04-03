9e91234706ce47cea7a1c665ed47b92bf102d89c
Scott Doxey <neogeek0101@gmail.com>
2024-04-03 11:24:41 -0400

    Added colors to the build script.

--- bin/build.sh
+++ bin/build.sh
@@ -2,6 +2,10 @@
 
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
+REDON=$(tput setaf 1)
+GREENON=$(tput setaf 2)
+COLOROFF=$(tput sgr0)
+
 (
 
     mkdir -p build/
@@ -10,6 +14,8 @@ SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
     cd ..
 
+    printf "Building macOS libraries ... "
+
     g++ -std=c++17 -shared -fPIC -o build/libRhythmGameUtilities.dylib includes/RhythmGameUtilities/RhythmGameUtilities.cpp
 
     mkdir -p UnityPackage/Libs/macOS
@@ -17,5 +23,7 @@ SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
     cp build/libRhythmGameUtilities.dylib UnityPackage/Libs/macOS
     cp build/libRhythmGameUtilities.dylib RhythmGameUtilities/Libs/macOS
+
+    printf "%sDONE%s\n" "${GREENON}" "${COLOROFF}"
 
 )
