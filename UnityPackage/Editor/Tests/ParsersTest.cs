75e23d537ee6a7f11a3c0c00f5fa31ad3cc8732d
Scott Doxey <neogeek0101@gmail.com>
2024-04-15 10:05:56 -0400

    Simplified the return type.
    Updated tests.

--- UnityPackage/Editor/Tests/ParsersTest.cs
+++ UnityPackage/Editor/Tests/ParsersTest.cs
@@ -10,14 +10,30 @@ namespace RhythmGameUtilities.Tests
         [Test]
         public void TestParseSectionsFromChart()
         {
-            var sections = Parsers.ParseSectionsFromChart(MockData.MOCK_CHART_CONTENTS)
-                .ToDictionary(item => item.Key, x => x.Value);
+            var sections = Parsers.ParseSectionsFromChart(MockData.MOCK_CHART_CONTENTS);
 
             Assert.That(sections.ContainsKey(NamedSection.Song), Is.True);
+            Assert.That(sections["Song"].Length, Is.EqualTo(12));
+            Assert.That(sections.ContainsKey(NamedSection.SyncTrack), Is.True);
+            Assert.That(sections[NamedSection.SyncTrack].Length, Is.EqualTo(11));
+            Assert.That(sections.ContainsKey(NamedSection.Events), Is.True);
+            Assert.That(sections[NamedSection.Events].Length, Is.EqualTo(16));
             Assert.That(sections.ContainsKey("ExpertSingle"), Is.True);
+            Assert.That(sections["ExpertSingle"].Length, Is.EqualTo(11));
+        }
 
-            Assert.That(sections["Song"].Count(), Is.EqualTo(12));
-            Assert.That(sections["ExpertSingle"].Count(), Is.EqualTo(11));
+        [Test]
+        public void TestParseValuesFromChartSections()
+        {
+            var sections = Parsers.ParseSectionsFromChart(MockData.MOCK_CHART_CONTENTS);
+
+            Assert.That(sections.ContainsKey(NamedSection.Song), Is.True);
+            Assert.That(sections[NamedSection.Song][0].Key, Is.EqualTo("Name"));
+            Assert.That(sections[NamedSection.Song][0].Value[0], Is.EqualTo("Example Song"));
+            Assert.That(sections[NamedSection.Song][6].Key, Is.EqualTo("Resolution"));
+            Assert.That(sections[NamedSection.Song][6].Value[0], Is.EqualTo("192"));
+            Assert.That(sections[NamedSection.Song][11].Key, Is.EqualTo("MusicStream"));
+            Assert.That(sections[NamedSection.Song][11].Value[0], Is.EqualTo("Example Song.ogg"));
         }
 
         [Test]
