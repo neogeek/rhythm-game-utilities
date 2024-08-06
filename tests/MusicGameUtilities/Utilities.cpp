cc18aa2ec4e17c504085cfe43076794d7c11c2cd
Scott Doxey <neogeek0101@gmail.com>
2024-08-06 08:30:32 -0400

    Updated test.

--- tests/MusicGameUtilities/Utilities.cpp
+++ tests/MusicGameUtilities/Utilities.cpp
@@ -1,6 +1,7 @@
 #include <cassert>
 #include <cmath>
 #include <iostream>
+#include <tuple>
 
 #include "RhythmGameUtilities/Utilities.h"
 
@@ -80,6 +81,8 @@ void testGenerateAdjacentKeyPairs()
         GenerateAdjacentKeyPairs(std::map<int, int>{{1, 2}, {3, 4}, {5, 6}});
 
     assert(adjacentKeyPairs.size() == 2);
+    assert(adjacentKeyPairs[0] == std::make_tuple(1, 3));
+    assert(adjacentKeyPairs[1] == std::make_tuple(3, 5));
 
     std::cout << ".";
 }
