aba2c8e11f4c6b36a8facb01812ba91592c299b7
Scott Doxey <neogeek0101@gmail.com>
2024-11-03 14:34:38 -0500

    Added additional methods.

--- GodotPlugin/include/rhythm_game_utilities.cpp
+++ GodotPlugin/include/rhythm_game_utilities.cpp
@@ -1,9 +1,12 @@
 #include "rhythm_game_utilities.h"
 
 #include <RhythmGameUtilities/Common.hpp>
+#include <RhythmGameUtilities/Utilities.hpp>
 
 void rhythm_game_utilities::_bind_methods()
 {
+    // Common
+
     ClassDB::bind_static_method("rhythm_game_utilities",
                                 D_METHOD("lerp", "a", "b", "t"),
                                 &rhythm_game_utilities::lerp);
@@ -11,7 +14,32 @@ void rhythm_game_utilities::_bind_methods()
     ClassDB::bind_static_method("rhythm_game_utilities",
                                 D_METHOD("inverse_lerp", "a", "b", "v"),
                                 &rhythm_game_utilities::inverse_lerp);
+
+    // Utilities
+
+    ClassDB::bind_static_method(
+        "rhythm_game_utilities",
+        D_METHOD("convert_tick_to_position", "tick", "resolution"),
+        &rhythm_game_utilities::convert_tick_to_position);
+
+    ClassDB::bind_static_method(
+        "rhythm_game_utilities",
+        D_METHOD("is_on_the_beat", "bpm", "currentTime", "delta"),
+        &rhythm_game_utilities::is_on_the_beat);
+
+    ClassDB::bind_static_method(
+        "rhythm_game_utilities",
+        D_METHOD("round_up_to_the_nearest_multiplier", "value", "multiplier"),
+        &rhythm_game_utilities::round_up_to_the_nearest_multiplier);
+
+    ClassDB::bind_static_method(
+        "rhythm_game_utilities",
+        D_METHOD("calculate_accuracy_ratio", "position", "currentPosition",
+                 "delta"),
+        &rhythm_game_utilities::calculate_accuracy_ratio);
 }
+
+// Common
 
 float rhythm_game_utilities::lerp(float a, float b, float t)
 {
@@ -21,4 +49,32 @@ float rhythm_game_utilities::lerp(float a, float b, float t)
 float rhythm_game_utilities::inverse_lerp(float a, float b, float v)
 {
     return RhythmGameUtilities::InverseLerp(a, b, v);
+}
+
+// Utilities
+
+float rhythm_game_utilities::convert_tick_to_position(int tick, int resolution)
+{
+    return RhythmGameUtilities::ConvertTickToPosition(tick, resolution);
+}
+
+bool rhythm_game_utilities::is_on_the_beat(int bpm, float currentTime,
+                                           float delta)
+{
+    return RhythmGameUtilities::IsOnTheBeat(bpm, currentTime, delta);
+}
+
+int rhythm_game_utilities::round_up_to_the_nearest_multiplier(int value,
+                                                              int multiplier)
+{
+    return RhythmGameUtilities::RoundUpToTheNearestMultiplier(value,
+                                                              multiplier);
+}
+
+float rhythm_game_utilities::calculate_accuracy_ratio(int position,
+                                                      int currentPosition,
+                                                      int delta)
+{
+    return RhythmGameUtilities::CalculateAccuracyRatio(position,
+                                                       currentPosition, delta);
 }
--- GodotPlugin/include/rhythm_game_utilities.h
+++ GodotPlugin/include/rhythm_game_utilities.h
@@ -14,7 +14,20 @@ class rhythm_game_utilities : public Object
     static void _bind_methods();
 
   public:
+    // Common
+
     static float lerp(float a, float b, float t);
 
     static float inverse_lerp(float a, float b, float v);
+
+    // Utilities
+
+    static float convert_tick_to_position(int tick, int resolution);
+
+    static bool is_on_the_beat(int bpm, float currentTime, float delta = 0.05f);
+
+    static int round_up_to_the_nearest_multiplier(int value, int multiplier);
+
+    static float calculate_accuracy_ratio(int position, int currentPosition,
+                                          int delta = 50);
 };
