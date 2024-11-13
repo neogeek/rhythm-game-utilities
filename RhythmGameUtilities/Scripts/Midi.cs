935e24b052ef8cb51ffca528cc263c6506c38f45
Scott Doxey <neogeek0101@gmail.com>
2024-11-13 01:52:20 -0500

    Added ReadMidiFile method.

---
+++ RhythmGameUtilities/Scripts/Midi.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace RhythmGameUtilities
+{
+
+    internal static class MidiInternal
+    {
+
+        [DllImport("libRhythmGameUtilities", CallingConvention = CallingConvention.Cdecl)]
+        public static extern IntPtr ReadMidiFileInternal(string filename, out int size);
+
+    }
+
+    public static class Midi
+    {
+
+        public static List<Note> ReadMidiFile(string path)
+        {
+            var notes = new List<Note>();
+
+            var ptrArray = MidiInternal.ReadMidiFileInternal(path, out var size);
+
+            var noteSize = Marshal.SizeOf(typeof(Note));
+
+            for (var i = 0; i < size; i += 1)
+            {
+                var noteSizePtr = new IntPtr(ptrArray.ToInt64() + noteSize * i);
+                var note = Marshal.PtrToStructure<Note>(noteSizePtr);
+
+                notes.Add(note);
+            }
+
+            Marshal.FreeHGlobal(ptrArray);
+
+            return notes;
+        }
+
+    }
+
+}
