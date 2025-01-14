096b6a871710eeb065822633fe4692f6e4c7687a
Scott Doxey <neogeek0101@gmail.com>
2025-01-14 02:11:33 -0500

    Added struct equality.

--- UnityPackage/Structs/Note.cs
+++ UnityPackage/Structs/Note.cs
@@ -1,10 +1,11 @@
+using System;
 using System.Runtime.InteropServices;
 
 namespace RhythmGameUtilities
 {
 
     [StructLayout(LayoutKind.Sequential)]
-    public struct Note
+    public struct Note : IEquatable<Note>
     {
 
         public int Position;
@@ -12,6 +13,28 @@ namespace RhythmGameUtilities
         public int HandPosition;
 
         public int Length;
+
+        public override int GetHashCode() => (Position, HandPosition, Length).GetHashCode();
+
+        public bool Equals(Note other)
+        {
+            return Position == other.Position && HandPosition == other.HandPosition && Length == other.Length;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Note other && Equals(other);
+        }
+
+        public static bool operator ==(Note left, Note right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(Note left, Note right)
+        {
+            return !(left == right);
+        }
 
     }
 
