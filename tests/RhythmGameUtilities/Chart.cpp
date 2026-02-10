2cd7826842c358753ac0f65761dfe061ccd57ac7
Scott Doxey <neogeek0101@gmail.com>
2026-02-10 00:00:44 -0500

    Fetch mock from file rather than inline string.

--- tests/RhythmGameUtilities/Chart.cpp
+++ tests/RhythmGameUtilities/Chart.cpp
@@ -2,77 +2,16 @@
 #include <cstring>
 #include <iostream>
 
-#include "RhythmGameUtilities/Enums/Difficulty.hpp"
+#include "RhythmGameUtilities/File.hpp"
 #include "RhythmGameUtilities/Parsers/Chart.hpp"
 
 using namespace RhythmGameUtilities;
 
-auto contents = R"([Song]
-{
-  Name = "Example Song"
-  Artist = "Example Artist"
-  Album = "Example Album"
-  Genre = "Example Genre"
-  Year = ", 2021"
-  Charter = "Example Charter"
-  Resolution = 192
-  Difficulty = 4
-  Offset = 0.56
-  PreviewStart = 45.28
-  PreviewEnd = 75.28
-  MusicStream = "Example Song.ogg"
-}
-[SyncTrack]
-{
-  0 = TS 4
-  0 = B 88000
-  3840 = B 112000
-  9984 = TS 2 1
-  9984 = B 89600
-  22272 = TS 4
-  22272 = B 112000
-  33792 = B 111500
-  34560 = B 112000
-  42240 = B 111980
-  69120 = TS 2 1
-}
-[Events]
-{
-  768 = E "section Intro"
-  9984 = E "phrase_start"
-  9984 = E "section Verse 1"
-  10080 = E "lyric Stand"
-  10208 = E "lyric in"
-  10344 = E "lyric line"
-  10496 = E "lyric as"
-  10608 = E "lyric we"
-  10736 = E "lyric march"
-  10896 = E "lyric to"
-  11008 = E "lyric the"
-  11112 = E "lyric drums"
-  11268 = E "lyric of"
-  11400 = E "lyric the"
-  11520 = E "lyric east"
-  11904 = E "phrase_end"
-}
-[ExpertSingle]
-{
-  768 = N 0 0
-  768 = S 64 768
-  864 = N 1 0
-  864 = N 5 0
-  960 = N 2 0
-  960 = N 6 0
-  1056 = N 3 0
-  1056 = E solo
-  1152 = N 4 0
-  1248 = N 7 0
-  1248 = E soloend
-})";
-
 void testReadResolutionFromChartData()
 {
-    auto resolution = ReadResolutionFromChartData(contents);
+    auto contents = ReadStringFromFile("./tests/Mocks/song.chart");
+
+    auto resolution = ReadResolutionFromChartData(contents.c_str());
 
     assert(resolution == 192);
 
@@ -81,7 +20,9 @@ void testReadResolutionFromChartData()
 
 void testReadTempoChangesFromChartData()
 {
-    auto tempoChanges = ReadTempoChangesFromChartData(contents);
+    auto contents = ReadStringFromFile("./tests/Mocks/song.chart");
+
+    auto tempoChanges = ReadTempoChangesFromChartData(contents.c_str());
 
     assert(tempoChanges.size() == 7);
 
@@ -90,7 +31,10 @@ void testReadTempoChangesFromChartData()
 
 void testReadTimeSignatureChangesFromChartData()
 {
-    auto timeSignatureChanges = ReadTimeSignatureChangesFromChartData(contents);
+    auto contents = ReadStringFromFile("./tests/Mocks/song.chart");
+
+    auto timeSignatureChanges =
+        ReadTimeSignatureChangesFromChartData(contents.c_str());
 
     assert(timeSignatureChanges.size() == 4);
 
@@ -99,7 +43,9 @@ void testReadTimeSignatureChangesFromChartData()
 
 void testReadNotesFromChartData()
 {
-    auto notes = ReadNotesFromChartData(contents, Difficulty::Expert);
+    auto contents = ReadStringFromFile("./tests/Mocks/song.chart");
+
+    auto notes = ReadNotesFromChartData(contents.c_str(), Difficulty::Expert);
 
     assert(notes.size() == 8);
 
@@ -108,7 +54,9 @@ void testReadNotesFromChartData()
 
 void testReadLyricsFromChartData()
 {
-    auto lyrics = ReadLyricsFromChartData(contents);
+    auto contents = ReadStringFromFile("./tests/Mocks/song.chart");
+
+    auto lyrics = ReadLyricsFromChartData(contents.c_str());
 
     assert(lyrics.size() == 12);
 
