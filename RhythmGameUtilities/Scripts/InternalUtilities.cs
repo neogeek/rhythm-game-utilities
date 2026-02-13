0ec1eebade94038d44f33ee969899ab2929daa1e
Scott Doxey <neogeek0101@gmail.com>
2026-02-13 00:02:10 -0500

    Changed syntax of namespace.

--- RhythmGameUtilities/Scripts/InternalUtilities.cs
+++ RhythmGameUtilities/Scripts/InternalUtilities.cs
@@ -1,25 +1,28 @@
 using System;
 using System.Runtime.InteropServices;
 
-namespace RhythmGameUtilities;
-
-public static class InternalUtilities
+namespace RhythmGameUtilities
 {
 
-    public static T[] CaptureArrayFromInternalMethod<T>(IntPtr ptrArray, int size)
+    public static class InternalUtilities
     {
-        var array = new T[size];
 
-        var structSize = Marshal.SizeOf(typeof(T));
-
-        for (var i = 0; i < size; i += 1)
+        public static T[] CaptureArrayFromInternalMethod<T>(IntPtr ptrArray, int size)
         {
-            var structSizePtr = new IntPtr(ptrArray.ToInt64() + structSize * i);
+            var array = new T[size];
 
-            array[i] = Marshal.PtrToStructure<T>(structSizePtr);
+            var structSize = Marshal.SizeOf(typeof(T));
+
+            for (var i = 0; i < size; i += 1)
+            {
+                var structSizePtr = new IntPtr(ptrArray.ToInt64() + structSize * i);
+
+                array[i] = Marshal.PtrToStructure<T>(structSizePtr);
+            }
+
+            return array;
         }
 
-        return array;
     }
 
 }
