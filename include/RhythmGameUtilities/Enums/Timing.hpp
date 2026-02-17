5a796080e1c3cebdede130d77e849c70b79c63a1
Scott Doxey <neogeek0101@gmail.com>
2026-02-16 21:09:22 -0500

    Added accuracy and timing enums.

---
+++ include/RhythmGameUtilities/Enums/Timing.hpp
@@ -0,0 +1,45 @@
+// Rhythm Game Utilities - https://github.com/neogeek/rhythm-game-utilities
+//
+// ░█▀▄░█░█░█░█░▀█▀░█░█░█▄█░░░█▀▀░█▀█░█▄█░█▀▀░░░█░█░▀█▀░▀█▀░█░░░▀█▀░▀█▀░▀█▀░█▀▀░█▀▀
+// ░█▀▄░█▀█░░█░░░█░░█▀█░█░█░░░█░█░█▀█░█░█░█▀▀░░░█░█░░█░░░█░░█░░░░█░░░█░░░█░░█▀▀░▀▀█
+// ░▀░▀░▀░▀░░▀░░░▀░░▀░▀░▀░▀░░░▀▀▀░▀░▀░▀░▀░▀▀▀░░░▀▀▀░░▀░░▀▀▀░▀▀▀░▀▀▀░░▀░░▀▀▀░▀▀▀░▀▀▀
+//
+// Copyright (c) Scott Doxey. All Rights Reserved. Licensed under the MIT
+// License. See LICENSE in the project root for license information.
+
+#pragma once
+
+#include <cstdint>
+#include <string>
+
+namespace RhythmGameUtilities
+{
+
+using Timing = enum : uint8_t {
+    Early,
+
+    Hit,
+
+    Late,
+
+    Miss
+};
+
+inline auto ToString(Timing timing) -> std::string
+{
+    switch (timing)
+    {
+    case Early:
+        return "Early";
+    case Hit:
+        return "Hit";
+    case Late:
+        return "Late";
+    case Miss:
+        return "Miss";
+    default:
+        return "Unknown Timing";
+    }
+}
+
+} // namespace RhythmGameUtilities
